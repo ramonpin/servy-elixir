19431b947e20fdf43d62e258c420a824c1345a33
ramonpin <ramon.pin@gmail.com>
2021-04-13 12:09:17 +0200

    feat: Disable log and track on test
    Log only on development and track 404s
    on development and production.

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -6,13 +6,17 @@ defmodule Servy.Plugins do
 
   @doc "Logs actual state of the conversantion."
   def log(%Conv{} = conv) do
-    Logger.info inspect(conv)
+    if Mix.env == :dev do
+      Logger.info inspect(conv)
+    end
     conv
   end
 
   @doc "Logs 404 requests."
   def track(%Conv{status: 404, path: path} = conv) do
-    Logger.warn "The path #{path} does not exists."
+    if Mix.env != :test do
+      Logger.warn "The path #{path} does not exists."
+    end
     conv
   end
 
