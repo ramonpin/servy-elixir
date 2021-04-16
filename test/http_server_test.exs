7df8f7a357b900d23de3b0a198ed3364159cf284
ramonpin <ramon.pin@gmail.com>
2021-04-16 18:54:29 +0200

    feat: Make the HttpServer test concurrent

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -3,6 +3,15 @@ defmodule HttpServerTest do
 
   alias Servy.HttpServer
 
+  defp send_request(parent, request_body) do
+    spawn fn -> send(parent, HTTPoison.post(
+      "http://localhost:4000/api/bears",
+      request_body,
+      [{"Content-Type", "application/json"}]
+    ))
+    end
+  end
+
   test "check our server replies to requests" do
     # Start our HttpServer
     pid = spawn(HttpServer, :start, [4000])
@@ -10,9 +19,16 @@ defmodule HttpServerTest do
     request_body = ~s({"name": "Baloo", "type": "Grizzly Pale"})
     response_body = ~s({"msg": "Created a Grizzly Pale bear named Baloo!"})
 
-    {:ok, response} = HTTPoison.post("http://localhost:4000/api/bears", request_body, [{"Content-Type", "application/json"}])
-    assert response.status_code == 201
-    assert response.body == response_body
+    Enum.each(1..5, fn _ ->
+      send_request(self(), request_body)
+    end)
+
+    Enum.each(1..5, fn _ -> receive do
+      {:ok, response} ->
+        assert response.status_code == 201
+        assert response.body == response_body
+      end
+    end)
 
     # End our HttpServer
     Process.exit(pid, "End Test")
